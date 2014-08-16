d63da333677d44b5e250c0516ec773948cbca33f
Hakimu Jackson <hakimu@newrelic.com>
2014-08-15 22:46:42 -0700

    Initial commit

---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'net/http'
+require 'json'
+require 'sinatra'
+require 'newrelic_rpm'
+
+get '/' do
+	'This works'
+end
+
+get '/:airport' do
+	@array = []
+	uri = Net::HTTP.get("services.faa.gov", "/airport/status/#{params[:airport].upcase}?format=application/json")
+	json = JSON.parse(uri)
+	@array << json["name"]
+	@array << json["weather"]["weather"]
+	@array << json["weather"]["temp"]
+	@array << json["status"]["reason"]
+	# @array.each {|x| print x }
+	@array
+end
+
+
+
+
+# pdx_uri = Net::HTTP.get('services.faa.gov','/airport/status/PDX?format=application/json')
+# lax_uri = Net::HTTP.get('services.faa.gov','/airport/status/SFO?format=application/json')
+
+# pdx_json = JSON.parse(pdx_uri)
+# lax_json = JSON.parse(lax_uri)
+
+
+# puts pdx_json["name"]
+# puts pdx_json["weather"]["weather"]
+# puts pdx_json["delay"]
+# puts "\n"
+# puts lax_json["name"]
+# puts lax_json["weather"]["weather"]
+# puts lax_json["weather"]["temp"]
+# puts lax_json["delay"]
+# puts lax_json["status"]["reason"]
